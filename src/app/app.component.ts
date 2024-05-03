ddec81a766c7e600b401c5e14709e033571fc916
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-05-03 18:58:29 +0200

    included the logo bar in the main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogoBarComponent } from "./logo-bar/logo-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogoBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
