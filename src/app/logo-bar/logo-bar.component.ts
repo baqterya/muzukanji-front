d8dbf27d56a55caeff33bc565f52df8383806242
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-05-04 16:02:36 +0200

    setting up the navigation buttons

--- src/app/logo-bar/logo-bar.component.ts
+++ src/app/logo-bar/logo-bar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {MatTab, MatTabGroup, MatTabLink, MatTabNav} from "@angular/material/tabs";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-logo-bar',
   standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
+    imports: [
+        NgOptimizedImage,
+        MatTabGroup,
+        MatTab,
+        MatTabNav,
+        MatTabLink,
+        MatButton
+    ],
   templateUrl: './logo-bar.component.html',
   styleUrl: './logo-bar.component.scss'
 })
