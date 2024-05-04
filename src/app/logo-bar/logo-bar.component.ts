855f2d3c2d21fa718c23b05d8e161ebc4ff83c56
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-05-04 22:16:09 +0200

    finished the logo-bar, for now at least

--- src/app/logo-bar/logo-bar.component.ts
+++ src/app/logo-bar/logo-bar.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
-import {MatTab, MatTabGroup, MatTabLink, MatTabNav} from "@angular/material/tabs";
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
+
+
 
 @Component({
   selector: 'app-logo-bar',
   standalone: true,
     imports: [
         NgOptimizedImage,
-        MatTabGroup,
-        MatTab,
-        MatTabNav,
-        MatTabLink,
-        MatButton
+        MatButton,
+        MatIconButton
     ],
   templateUrl: './logo-bar.component.html',
   styleUrl: './logo-bar.component.scss'
