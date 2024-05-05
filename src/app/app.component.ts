73267880ed1b944fce8d0d86154d26df6b2a02a6
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-05-05 17:09:01 +0200

    refactored all files to use 4 space tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LogoBarComponent } from "./logo-bar/logo-bar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, LogoBarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, LogoBarComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'muzukanji-front';
+    title = 'muzukanji-front';
 }
--- src/app/logo-bar/logo-bar.component.ts
+++ src/app/logo-bar/logo-bar.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
-import {MatButton, MatIconButton} from "@angular/material/button";
-
+import { NgOptimizedImage } from "@angular/common";
+import { MatButton, MatIconButton } from "@angular/material/button";
 
 
 @Component({
-  selector: 'app-logo-bar',
-  standalone: true,
+    selector: 'app-logo-bar',
+    standalone: true,
     imports: [
         NgOptimizedImage,
         MatButton,
         MatIconButton
     ],
-  templateUrl: './logo-bar.component.html',
-  styleUrl: './logo-bar.component.scss'
+    templateUrl: './logo-bar.component.html',
+    styleUrl: './logo-bar.component.scss'
 })
 export class LogoBarComponent {
 
