76cc1c71e0cbcd3970b67d42f3200aa0b389f711
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-05-05 18:59:58 +0200

    added try-it-body to the router, added the router link to the try-it button in the nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FrontPageBodyComponent } from "./front-page-body/front-page-body.component";
+import { TryItBodyComponent } from "./try-it-body/try-it-body.component";
 
 export const routes: Routes = [
-    { path: 'front-page', component: FrontPageBodyComponent }
+    { path: 'front-page', component: FrontPageBodyComponent },
+    { path: 'try-it-page', component: TryItBodyComponent }
 ];
