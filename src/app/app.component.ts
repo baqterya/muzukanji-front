c541328140261225d9468a236cca452e18a904e4
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-05-05 17:34:11 +0200

    included front-page-body.component in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogoBarComponent } from "./logo-bar/logo-bar.component";
+import {FrontPageBodyComponent} from "./front-page-body/front-page-body.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, LogoBarComponent],
+    imports: [RouterOutlet, LogoBarComponent, FrontPageBodyComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
