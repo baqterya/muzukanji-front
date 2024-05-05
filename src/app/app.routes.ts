743d3091fe6db4248e560cf9b1bfb6b2bd2fdc63
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-05-05 18:27:23 +0200

    added the front-page to the router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FrontPageBodyComponent } from "./front-page-body/front-page-body.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'front-page', component: FrontPageBodyComponent }
+];
