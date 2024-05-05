885500c9bf8bd4984c52a437f4596f5ff098bebe
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-05-05 18:07:40 +0200

    created the footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogoBarComponent } from "./logo-bar/logo-bar.component";
 import {FrontPageBodyComponent} from "./front-page-body/front-page-body.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, LogoBarComponent, FrontPageBodyComponent],
+    imports: [RouterOutlet, LogoBarComponent, FrontPageBodyComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
